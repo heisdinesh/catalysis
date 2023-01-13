2d75e4c260a0caed4701a72b5492dbce8c5412a4
Abhinav Mishra <abhinavmishraise@gmail.com>
2023-01-13 22:11:00 +0530

    First Commit

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -2,8 +2,41 @@ import React from 'react'
 
 const Events = () => {
   return (
-    <div>Events</div>
-  )
+        <div className="grid grid-cols-4 gap-4" style={{backgroundColor: "#071E22"}}>
+            <div>
+                <img src="logo192.png" alt="Caption 1" className="w-full" />
+                <p className="text-center pt-2">Caption 1</p>
+            </div>
+            <div>
+                <img src="logo192.png" alt="Caption 2" className="w-full" />
+                <p className="text-center pt-2">Caption 2</p>
+            </div>
+            <div>
+                <img src="logo192.png" alt="Caption 3" className="w-full" />
+                <p className="text-center pt-2">Caption 3</p>
+            </div>
+            <div>
+                <img src="logo192.png" alt="Caption 4" className="w-full" />
+                <p className="text-center pt-2">Caption 4</p>
+            </div>
+            <div>
+                <img src="logo192.png" alt="Caption 5" className="w-full" />
+                <p className="text-center pt-2">Caption 5</p>
+            </div>
+            <div>
+                <img src="logo192.png" alt="Caption 6" className="w-full" />
+                <p className="text-center pt-2">Caption 6</p>
+            </div>
+            <div>
+                <img src="logo192.png" alt="Caption 7" className="w-full" />
+                <p className="text-center pt-2">Caption 7</p>
+            </div>
+            <div>
+                <img src="logo192.png" alt="Caption 8" className="w-full" />
+                <p className="text-center pt-2">Caption 8</p>
+            </div>
+        </div>
+    );
 }
 
 export default Events
