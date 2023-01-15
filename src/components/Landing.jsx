af76176fe96249f9d3165c5bde56403e7719dbd5
Abhinav Mishra <abhinavmishraise@gmail.com>
2023-01-16 00:13:12 +0530

    Landing Responsive

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -6,11 +6,11 @@ const Landing = () => {
         <section className="bg-background text-fontcolor body-font">
           <div className="items-center justify-center">
               <div className="items-center justify-center">
-                  <img className="px-72 pt-28" alt="Poster1" src={require('../assets/catalysis2.png')}/>
+                  <img className="sm:px-[20%] px-[10%] pt-28" alt="Poster1" src={require('../assets/catalysis2.png')}/>
               </div>
-              <h1 className="font-bold mb-8 text-center justify-center text-5xl" style={{color: '#E5F2FF'}}>23 - 25 January 2023</h1>
+              <h1 className="font-bold mb-8 text-center justify-center sm:text-5xl text-3xl" style={{color: '#E5F2FF'}}>23 - 25 January 2023</h1>
               <div className="flex justify-center">
-                  <button className="pt-1 pb-2 px-8 rounded-lg mr-8"  style={{backgroundColor: "#00FDFB"}}>
+                  <button className="pt-1 pb-2 sm:px-8 px-[10%] rounded-lg sm:mr-8"  style={{backgroundColor: "#00FDFB"}}>
                       <a href="index.js" className="text-[#071E22] font-medium text-xl">Register</a>
                   </button>
               </div>
