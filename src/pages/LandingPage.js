253c16190402a0ddc4c6ab76ec3fc12148ce8541
Astraxx04 <gagan200254@gmail.com>
2023-01-19 00:42:32 +0530

    Final Registration

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -7,7 +7,7 @@ const LandingPage = () => {
         <Landing />
         <About />
         <Events />
-        {/*<Registeration />*/}
+        <Registeration />
         {/*<Sponsers />*/}
         <FAQ />
         <Footer />
