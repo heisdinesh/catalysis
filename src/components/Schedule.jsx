f07decddd16f5b4981d940d891cd6373f810bc9d
Astraxx04 <gagan200254@gmail.com>
2023-01-19 01:31:36 +0530

    Table data

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -2,7 +2,89 @@ import React from 'react'
 
 const Schedule = () => {
   return (
-    <div>Schedule</div>
+    <div>
+        <div className='bg-[#091C22] lg:max-w-full lg:py-12 lg:px-16 md:py-16 md:px-16 py-5 px-5'>
+        <div className='text-[#E5F2FF] lg:font-bold lg:text-left lg:text-center lg:text-5xl lg:p-10'>Schedule</div>
+            <table class="border-collapse border border-slate-500 text-[#E5F2FF]">
+                <thead>
+                    <tr>
+                    <th class="border border-slate-600 ">Date</th>
+                    <th class="border border-slate-600 ">Event</th>
+                    <th class="border border-slate-600 ">Time</th>
+                    <th class="border border-slate-600 ">Venue</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td class="border border-slate-700 ">23-01-2022</td>
+                    <td class="border border-slate-700 ">Inaguration</td>
+                    <td class="border border-slate-700 ">09:00-11:00</td>
+                    <td class="border border-slate-700 ">ISE</td>
+                    </tr>
+                    <tr>
+                    <td class="border border-slate-700 ">23-01-2022</td>
+                    <td class="border border-slate-700 ">Air Crash</td>
+                    <td class="border border-slate-700 ">02:00-05:00</td>
+                    <td class="border border-slate-700 ">Amphi Theater</td>
+                    </tr>
+                    <tr>
+                    <td class="border border-slate-700 ">23-01-2022</td>
+                    <td class="border border-slate-700 ">Quizz Up</td>
+                    <td class="border border-slate-700 ">11:00-04:00</td>
+                    <td class="border border-slate-700 ">#009 and #205</td>
+                    </tr>
+                    <tr>
+                    <td class="border border-slate-700 ">23-01-2022</td>
+                    <td class="border border-slate-700 ">Coding JAM(Coding Contest)</td>
+                    <td class="border border-slate-700 ">02:00-05:00</td>
+                    <td class="border border-slate-700 ">All four labs</td>
+                    </tr>
+                    <tr>
+                    <td class="border border-slate-700 ">24-01-2022</td>
+                    <td class="border border-slate-700 ">What's the Point(Debate)</td>
+                    <td class="border border-slate-700 ">11:00-02:00</td>
+                    <td class="border border-slate-700 ">Amphi Theater</td>
+                    </tr>
+                    <tr>
+                    <td class="border border-slate-700 ">24-01-2022</td>
+                    <td class="border border-slate-700 ">Innovator(JAM)</td>
+                    <td class="border border-slate-700 ">02:00-05:00</td>
+                    <td class="border border-slate-700 ">Amphi Theater</td>
+                    </tr>
+                    <tr>
+                    <td class="border border-slate-700 ">24-01-2022</td>
+                    <td class="border border-slate-700 ">Call to Launch(Pitch an Idea)</td>
+                    <td class="border border-slate-700 ">10:00-01:00</td>
+                    <td class="border border-slate-700 ">#009 and #205</td>
+                    </tr>
+                    <tr>
+                    <td class="border border-slate-700 ">25-01-2022</td>
+                    <td class="border border-slate-700 ">Code Red(Valorant Gaming)</td>
+                    <td class="border border-slate-700 ">10:00-02:00</td>
+                    <td class="border border-slate-700 ">Online</td>
+                    </tr>
+                    <tr>
+                    <td class="border border-slate-700 ">25-01-2022</td>
+                    <td class="border border-slate-700 ">Techno Seek</td>
+                    <td class="border border-slate-700 ">12:00-03:00</td>
+                    <td class="border border-slate-700 ">Collage Campus</td>
+                    </tr>
+                    <tr>
+                    <td class="border border-slate-700 ">25-01-2022</td>
+                    <td class="border border-slate-700 ">Prize Distribution</td>
+                    <td class="border border-slate-700 ">03:00-04:00</td>
+                    <td class="border border-slate-700 ">College Campus</td>
+                    </tr>
+                    <tr>
+                    <td class="border border-slate-700 ">25-01-2022</td>
+                    <td class="border border-slate-700 ">Closing Ceremony</td>
+                    <td class="border border-slate-700 ">04:00-04:30</td>
+                    <td class="border border-slate-700 ">ISE</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
   )
 }
 
