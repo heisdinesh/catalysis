71dbb247c1ebeb969effcd2114ac21d4c5504980
suryansh <suryanshgupta03@gmail.com>
2023-05-29 00:16:33 +0530

    added new gifs and made some changes in events

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import Lottie from 'lottie-react'
+import contact_gif from '../assets/contact_gif.json'
 const Footer = () => {
     // const [copied, setCopied] = useState(false);
 
@@ -7,35 +8,35 @@ const Footer = () => {
         navigator.clipboard.writeText("ise.genesis.dsce@gmail.com");
         // setCopied(true);
     };
-  return (
-      <div className="bg-[#071E22] mt-1" id='contact'>
-          <h1 className="font-bold text-center text-5xl p-8" style={{color:'#E5F2FF'}}>Contact Us</h1>
-          <footer className="text-center text-white bg-[#071E22] sm:px-[20%] px-[10%]">
-              <div className="container pt-9 flex justify-center mb-9">
-                  <a href="https://instagram.com/genesis.ise" target="_blank" className="mr-9">
-                      <img src={require('../assets/instagram.png')} alt="Logo" className="h-14" />
-                  </a>
-                  <a href="https://www.linkedin.com/company/gensis-ise-dsce/" target="_blank" className="mr-9">
-                      <img src={require('../assets/linkedin.png')} alt="Logo" className="h-14" />
-                  </a>
-                  {/*<a href="index.js" className="mr-9">*/}
-                  {/*    <img src={require('../assets/twitter.png')} alt="Logo" className="h-14" />*/}
-                  {/*</a>*/}
-                  <a href="mailto:ise.genesis.dsce@gmail.com" target="_blank" onClick={handleCopy} className="text-gray-800">
-                      <img src={require('../assets/email.png')} alt="Logo" className="h-14" />
-                  </a>
-              </div>
+    return (
+        <div className="bg-[#071E22] mt-1" id='contact'>
+            <h1 className="font-bold text-center text-5xl p-8" style={{ color: '#E5F2FF' }}>Contact Us<Lottie className='w-1/4 mt-4 mx-auto' animationData={contact_gif} /></h1>
+            <footer className="text-center text-white bg-[#071E22] sm:px-[20%] px-[10%]">
+                <div className="container mt-[-1.5rem] pt-9 flex justify-center mb-9">
+                    <a href="https://instagram.com/genesis.ise" target="_blank" className="mr-9">
+                        <img src={require('../assets/instagram.png')} alt="Logo" className="h-14" />
+                    </a>
+                    <a href="https://www.linkedin.com/company/gensis-ise-dsce/" target="_blank" className="mr-9">
+                        <img src={require('../assets/linkedin.png')} alt="Logo" className="h-14" />
+                    </a>
+                    {/*<a href="index.js" className="mr-9">*/}
+                    {/*    <img src={require('../assets/twitter.png')} alt="Logo" className="h-14" />*/}
+                    {/*</a>*/}
+                    <a href="mailto:ise.genesis.dsce@gmail.com" target="_blank" onClick={handleCopy} className="text-gray-800">
+                        <img src={require('../assets/email.png')} alt="Logo" className="h-14" />
+                    </a>
+                </div>
 
-              <div className="text-center py-4 text-[#E5F2FF] text-xl">
-                  70906 88613 | 73384 98081
-              </div>
+                <div className="text-center py-4 text-[#E5F2FF] text-xl">
+                    70906 88613 | 73384 98081
+                </div>
 
-              <div className="text-center py-4 text-[#E5F2FF]">
-                  Copyright © 2023 All rights reserved | Made by genesis team.
-              </div>
-          </footer>
-      </div>
-  )
+                <div className="text-center py-4 text-[#E5F2FF]">
+                    Copyright © 2023 All rights reserved | Made by genesis team.
+                </div>
+            </footer>
+        </div>
+    )
 }
 
 export default Footer
--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,91 +1,92 @@
 import React from 'react'
-
+import Lottie from 'lottie-react'
+import schedule_gif from '../assets/schedule_gif.json'
 const Schedule = () => {
-  return (
-    <div id='schedule'>
-        <div className='bg-[#091C22] lg:max-w-full lg:py-12 lg:px-16 md:py-9 md:px-16 py-5 px-5'>
-        <div className='text-[#E5F2FF] lg:font-bold lg:text-center lg:text-5xl lg:p-10 md:font-bold md:text-center md:text-5xl md:p-5 font-bold text-center text-5xl p-5 '>Schedule</div>
-            <table class="border-collapse border border-slate-500 text-[#E5F2FF] mx-auto border-spacing-9 font-serif ">
-                <thead>
-                    <tr>
-                    <th class="border-4 border-[#93a3eb] lg:text-5xl lg:w-[220px] p-3">Date</th>
-                    <th class="border-4 border-[#93a3eb] lg:text-5xl lg:w-[320px] p-3">Event</th>
-                    <th class="border-4 border-[#93a3eb] lg:text-5xl lg:w-[240px] p-3">Time</th>
-                    <th class="border-4 border-[#93a3eb] lg:text-5xl lg:w-[360px] p-3">Venue</th>
-                    </tr>
-                </thead>
-                <tbody className='text-center lg:text-2xl lg:font-light '>
-                    <tr>
-                    <td class="border-4 border-[#93a3eb] p-2 ">23-01-2022</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">Inaguration</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">09:30-11:00</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">AV Auditorium</td>
-                    </tr>
-                    <tr>
-                    <td class="border-4 border-[#93a3eb] p-2 ">23-01-2022</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">Air Crash</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">14:00-17:00</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">Amphitheatre</td>
-                    </tr>
-                    <tr>
-                    <td class="border-4 border-[#93a3eb] p-2 ">23-01-2022</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">Quizz Up</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">11:00-16:00</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">#009 and #205 (ISE Dept)</td>
-                    </tr>
-                    <tr>
-                    <td class="border-4 border-[#93a3eb] p-2 ">23-01-2022</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">Coding JAM</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">14:00-17:00</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">All 4 Labs (ISE Dept)</td>
-                    </tr>
-                    <tr>
-                    <td class="border-4 border-[#93a3eb] p-2 ">24-01-2022</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">What's the Point?</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">11:00-14:00</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">Amphitheatre</td>
-                    </tr>
-                    <tr>
-                    <td class="border-4 border-[#93a3eb] p-2 ">24-01-2022</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">Innovator</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">14:00-17:00</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">Amphitheatre</td>
-                    </tr>
-                    <tr>
-                    <td class="border-4 border-[#93a3eb] p-2 ">24-01-2022</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">Call to Launch</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">10:00-13:00</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">#009 and #205 (ISE Dept)</td>
-                    </tr>
-                    <tr>
-                    <td class="border-4 border-[#93a3eb] p-2 ">25-01-2022</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">Code Red</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">10:00-14:00</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">Virtual Mode</td>
-                    </tr>
-                    <tr>
-                    <td class="border-4 border-[#93a3eb] p-2 ">25-01-2022</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">Techno Seek</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">12:00-15:00</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">DSCE Campus</td>
-                    </tr>
-                    <tr>
-                    <td class="border-4 border-[#93a3eb] p-2 ">25-01-2022</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">Prize Distribution</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">15:00-16:00</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">AV Auditorium</td>
-                    </tr>
-                    <tr>
-                    <td class="border-4 border-[#93a3eb] p-2 ">25-01-2022</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">Closing Ceremony</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">16:00-16:30</td>
-                    <td class="border-4 border-[#93a3eb] p-2 ">AV Auditorium</td>
-                    </tr>
-                </tbody>
-            </table>
+    return (
+        <div id='schedule'>
+            <div className='bg-[#091C22] lg:max-w-full lg:py-12 lg:px-16 md:py-9 md:px-16 py-5 px-5'>
+                <div className='text-[#E5F2FF] lg:font-bold lg:text-center lg:text-5xl lg:p-10 md:font-bold md:text-center md:text-5xl md:p-5 font-bold text-center text-5xl p-5 '>Schedule<Lottie className='w-1/4 mx-auto' animationData={schedule_gif} /></div>
+                <table class="border-collapse border border-slate-500 text-[#E5F2FF] mx-auto border-spacing-9 font-serif ">
+                    <thead>
+                        <tr>
+                            <th class="border-4 border-[#93a3eb] lg:text-5xl lg:w-[220px] p-3">Date</th>
+                            <th class="border-4 border-[#93a3eb] lg:text-5xl lg:w-[320px] p-3">Event</th>
+                            <th class="border-4 border-[#93a3eb] lg:text-5xl lg:w-[240px] p-3">Time</th>
+                            <th class="border-4 border-[#93a3eb] lg:text-5xl lg:w-[360px] p-3">Venue</th>
+                        </tr>
+                    </thead>
+                    <tbody className='text-center lg:text-2xl lg:font-light '>
+                        <tr>
+                            <td class="border-4 border-[#93a3eb] p-2 ">23-01-2022</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">Inaguration</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">09:30-11:00</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">AV Auditorium</td>
+                        </tr>
+                        <tr>
+                            <td class="border-4 border-[#93a3eb] p-2 ">23-01-2022</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">Air Crash</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">14:00-17:00</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">Amphitheatre</td>
+                        </tr>
+                        <tr>
+                            <td class="border-4 border-[#93a3eb] p-2 ">23-01-2022</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">Quizz Up</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">11:00-16:00</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">#009 and #205 (ISE Dept)</td>
+                        </tr>
+                        <tr>
+                            <td class="border-4 border-[#93a3eb] p-2 ">23-01-2022</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">Coding JAM</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">14:00-17:00</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">All 4 Labs (ISE Dept)</td>
+                        </tr>
+                        <tr>
+                            <td class="border-4 border-[#93a3eb] p-2 ">24-01-2022</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">What's the Point?</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">11:00-14:00</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">Amphitheatre</td>
+                        </tr>
+                        <tr>
+                            <td class="border-4 border-[#93a3eb] p-2 ">24-01-2022</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">Innovator</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">14:00-17:00</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">Amphitheatre</td>
+                        </tr>
+                        <tr>
+                            <td class="border-4 border-[#93a3eb] p-2 ">24-01-2022</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">Call to Launch</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">10:00-13:00</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">#009 and #205 (ISE Dept)</td>
+                        </tr>
+                        <tr>
+                            <td class="border-4 border-[#93a3eb] p-2 ">25-01-2022</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">Code Red</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">10:00-14:00</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">Virtual Mode</td>
+                        </tr>
+                        <tr>
+                            <td class="border-4 border-[#93a3eb] p-2 ">25-01-2022</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">Techno Seek</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">12:00-15:00</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">DSCE Campus</td>
+                        </tr>
+                        <tr>
+                            <td class="border-4 border-[#93a3eb] p-2 ">25-01-2022</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">Prize Distribution</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">15:00-16:00</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">AV Auditorium</td>
+                        </tr>
+                        <tr>
+                            <td class="border-4 border-[#93a3eb] p-2 ">25-01-2022</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">Closing Ceremony</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">16:00-16:30</td>
+                            <td class="border-4 border-[#93a3eb] p-2 ">AV Auditorium</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Schedule
